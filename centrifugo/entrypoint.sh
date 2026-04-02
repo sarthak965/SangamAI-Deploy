29393e2f5f2cf448dd2a048d5e70f2cd55dc79cf
Sarthak <sarthakchaudhary@zohomail.in>
2026-04-02 20:40:43 +0530

    new erros

--- centrifugo/entrypoint.sh
+++ centrifugo/entrypoint.sh
@@ -37,4 +37,4 @@ cat > /tmp/config.json <<EOF
 }
 EOF
 
-exec centrifugo serve -c /tmp/config.json --http_server.port "${PORT:-8000}"
+exec centrifugo --config=/tmp/config.json --http_server.port="${PORT:-8000}"
